70f670eb0940a837f466dfecbce7b997de518c83
nguyenhoangkhue <148615428+nguyenhoangkhue@users.noreply.github.com>
2023-11-09 20:59:29 +0700

    upd b8

--- b6/assets/js/custom-owl.js
+++ b6/assets/js/custom-owl.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+  $('.owl-carousel').owlCarousel({
+    loop:true,/* vong lap vo tan*/ 
+    margin:20,
+    nav:false,/* thanh dieu huong*/
+    dots: true,/* dau cham*/
+    autoplay: true, /*slide tu dong chay*/
+    autoplayTimeout: 2000,/*tgian tu chuyen slide*/
+    responsive:{ /* hien thi item tren cac khoang man hinh*/
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
   });
+  
